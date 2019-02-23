53f3897fdd16fca3198989d2d8e4e7040dd790f9
Yuki <yukimashiro425@gmail.com>
2019-02-22 21:25:12 -0500

    Asteroid
    Add asteroid rotation, and disappearing

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+       void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Boundary")
+            {
+                return;
+            }
+            if (other.tag == "Player")
+            {
+                Destroy(other.gameObject);
+            }
+            
+            Destroy(this.gameObject);  //Asteroid
+            Destroy(other.gameObject); //Laser
+        }
+    
+}
