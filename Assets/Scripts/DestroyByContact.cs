8cf6045766522635827ce744ac7e8c93bcec562e
SiyingLi <yukimashiro425@gmail.com>
2019-03-08 21:12:58 -0500

    Explosion Animation

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
-       void OnTriggerEnter2D(Collider2D other)
+
+    public GameObject explosion;
+    void OnTriggerEnter2D(Collider2D other)
         {
+
+           
+
             if (other.tag == "Boundary")
             {
                 return;
             }
             if (other.tag == "Player")
             {
+            //trigger game over logic in here
                 Destroy(other.gameObject);
             }
-            
+
+        //Instantiate asteroid explosion animation
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+
             Destroy(this.gameObject);  //Asteroid
             Destroy(other.gameObject); //Laser
         }
